61240b6b71d8fee9d06f0b64a9b7e605aba3647a
Chris Barnes <barnesc@janelia.hhmi.org>
2021-06-01 16:08:29 +0100

    Move readme tests into CI

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 
 import pytest
-from click.testing import CliRunner
-
-from ffs.cli import main
 
 PROJECT_DIR = Path(__file__).parent.parent
 SRC_DIR: Path = PROJECT_DIR / "data-policy"
@@ -25,29 +22,3 @@ def test_correct_documents(fname: str):
     tgt = TGT_DIR / fname
 
     assert src.read_text() == tgt.read_text()
-
-
-def normalise_whitespace(s: str):
-    return " ".join(s.split())
-
-
-@pytest.mark.parametrize(
-    ["cmd"],
-    [
-        ("",),
-        ("export",),
-        ("book",),
-        ("problems",),
-    ],
-)
-def test_correct_help(cmd):
-    readme_txt = normalise_whitespace((PROJECT_DIR / "README.md").read_text())
-    args = ["--help"]
-    if cmd:
-        args.insert(0, cmd)
-    runner = CliRunner()
-    result = runner.invoke(main, args)
-    assert result.exit_code == 0
-    msg = normalise_whitespace(result.output)
-    replaced = msg.replace("Usage: main ", "Usage: ffs ")
-    assert replaced in readme_txt
